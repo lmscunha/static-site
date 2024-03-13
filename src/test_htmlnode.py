3ecf155df16c0bb23c4554b8b7df2853afbe45e4
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-03-13 06:49:28 -0300

    test: finish initial tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -4,18 +4,27 @@ from htmlnode import HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
-    def test_eq(self):
+    def test_eq_button(self):
         node = HTMLNode("button", "Click",  props={"type": "button"})
         self.assertEqual(node.props_to_html(), ' type="button"')
 
-    # def test_is_none(self):
-    #     node = TextNode("This is a text node", "bold")
-    #     self.assertIsNone(node.url)
-    #
-    # def test_not_equal(self):
-    #     node = TextNode("This is a text node", "bold")
-    #     node2 = TextNode("This is a text node different", "bold")
-    #     self.assertNotEqual(node, node2)
+    def test_eq_a(self):
+        node = HTMLNode("a", "Website", props={"href": "https://website.com"})
+        self.assertEqual(node.props_to_html(), ' href="https://website.com"')
+
+    def test_eq_canvas(self):
+        node = HTMLNode("canvas", "Alternative text", props={
+                        "width": "120", "height": "80"})
+        self.assertEqual(node.props_to_html(), ' width="120" height="80"')
+
+    def test_is_none(self):
+        node = HTMLNode("a", "Website", props={"href": "https://website.com"})
+        self.assertIsNone(node.children)
+
+    def test_not_equal(self):
+        node = HTMLNode("a", "Website", props={"href": "https://website.com"})
+        node2 = HTMLNode("button", "Click",  props={"type": "button"})
+        self.assertNotEqual(node, node2)
 
     if __name__ == "__main__":
         unittest.main()
