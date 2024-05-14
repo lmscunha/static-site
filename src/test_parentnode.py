8d31cf8f717a3ca2c32b4cd1dd5fe203c1754386
Leonardo Cunha <agile.ice4816@fastmail.com>
2024-05-14 05:53:45 -0300

    test: add two  nested parent level

--- src/test_parentnode.py
+++ src/test_parentnode.py
@@ -25,5 +25,18 @@ class TestParentNode(unittest.TestCase):
         self.assertEqual(node.to_html(
         ), '<div><p><b>Bold text</b>Normal text<i>italic text</i>Normal text</p></div>')
 
+    def test_eq_to_children_to_html_nested_children(self):
+        node = ParentNode([ParentNode([
+            LeafNode("Bold text", "b",),
+            ParentNode([
+                LeafNode("Text Inside Parent", None,)
+            ], "p"),
+            LeafNode("Normal text", None,),
+            LeafNode("italic text", "i",),
+            LeafNode("Normal text", None),
+        ], "p")], "div")
+        self.assertEqual(node.to_html(
+        ), '<div><p><b>Bold text</b><p>Text Inside Parent</p>Normal text<i>italic text</i>Normal text</p></div>')
+
     if __name__ == "__main__":
         unittest.main()
